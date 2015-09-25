576f5ea29ce54b5fc7a063b9245466e9d24b0edf
Aaron Perl <perla@rpi.edu>
2015-09-25 15:33:16 -0400

    ACTUALLY added Module definitions header, oops

---
+++ include/ModuleDefines.hpp
@@ -0,0 +1,27 @@
+#ifndef __MODULEDEFINES_HPP__
+#define __MODULEDEFINES_HPP__
+
+namespace Module
+{
+	class ModuleGame;
+		class GameObject;
+		class Vector3;
+		class Quaternion;
+		class TransformObject;
+	class GraphicsInterface;
+		class GraphicsCallback;
+		class GraphicsContext;
+		class Mesh;
+		class Color;
+		class PolygonContainer;
+	class AudioInterface;
+		class Sound;
+		class SoundClip;
+	class ThreadingInterface;
+		class ThreadData;
+		class ThreadObject;
+		class Mutex;
+	//class NetworkInterface;
+}
+
+#endif
