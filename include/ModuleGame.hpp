8ea91f768045181d9d8b30d1ff074508201f7662
Aaron Perl <perla@rpi.edu>
2015-09-28 13:05:09 -0400

    Started documenting existing files with Doxygen comments

--- include/ModuleGame.hpp
+++ include/ModuleGame.hpp
@@ -10,17 +10,18 @@
 
 namespace Module {
 
-// The main class of Module. The ModuleGame class represents the core of Module. Its functionality ties together
-// that of the different interfaces. End users of Module (game developers) control Module through this class and not
-// by accessing interfaces, as those are meant to be abstracted away from users. Setting up a ModuleGame instance should
-// be the first thing done in a game that uses Module.
+/** The main class of Module. The ModuleGame class represents the core of Module. Its functionality ties together
+ * that of the different interfaces. End users of Module (game developers) control Module through this class and not
+ * by accessing interfaces, as those are meant to be abstracted away from users. Setting up a ModuleGame instance should
+ * be the first thing done in a game that uses Module.
+ */
 class ModuleGame {
 private:
-	GraphicsInterface* graphics;
-	AudioInterface* audio;
-	ThreadingInterface* threading;
+	GraphicsInterface* graphics; /**< The GraphicsInterface attached to this game */
+	AudioInterface* audio; /**< The AudioInterface attached to this game */
+	ThreadingInterface* threading; /**< The ThreadingInterface attached to this game */
 	
-	Book<GameObject> objects; // The book of all GameObjects
+	Book<GameObject> objects; /**< The book of all GameObjects */
 public:
 	ModuleGame();
 	
@@ -47,31 +48,52 @@ public:
 	unsigned long getMilliseconds();
 };
 
+/**
+ * @return The current number of GameObject instances in this game.
+ */
 inline Book<GameObject>::size_type ModuleGame::numObjects() const
 {
 	return objects.size();
 }
 
+/**
+ * @return A reference to the index'th GameObject in this game.
+ */
 inline GameObject* ModuleGame::getGameObject(Book<GameObject>::size_type index)
 {
 	return &objects[index];
 }
 
+/**
+ * @return A const reference to the index'th GameObject in this game.
+ */
 inline const GameObject* ModuleGame::getGameObject(Book<GameObject>::size_type index) const
 {
 	return &objects[index];
 }
 
+/**
+ * Creates a Mutex.
+ * @return A pointer to the newly created Mutex
+ */
 inline Mutex* ModuleGame::createMutex()
 {
 	return threading->createMutex();
 }
 
+/**
+ * Sets the Mesh component of a GameObject
+ * @param obj The GameObject to set the Mesh component of
+ * @param mesh The corresponding Mesh
+ */
 inline void ModuleGame::setMesh(GameObject* obj, Mesh* mesh)
 {
 	obj->mesh = mesh;
 }
 
+/**
+ * @return The number of milliseconds that have elapsed since the start of the current game.
+ */
 inline unsigned long ModuleGame::getMilliseconds()
 {
 	return graphics->getMilliseconds();
--- src/ModuleGame.cpp
+++ src/ModuleGame.cpp
@@ -1,40 +1,66 @@
 #include "ModuleGame.hpp"
 #include <cassert>
 
+/**
+ * The default constructor of ModuleGame.
+ */
 Module::ModuleGame::ModuleGame() : graphics(0), audio(0), threading(0)
 {
 }
 
+/**
+ * Creates a new GameObject.
+ * @return A pointer to the created GameObject
+ */
 Module::GameObject* Module::ModuleGame::createGameObject()
 {
 	objects.push_back(GameObject(this));
 	return &objects.back();
 }
 
+/**
+ * Attaches a GraphicsInterface to this game.
+ * @param newGraphics A pointer to the GraphicsInterface to attach to this game
+ */
 void Module::ModuleGame::attachGraphicsInterface(GraphicsInterface* newGraphics)
 {
 	newGraphics->game = this;
 	graphics = newGraphics;
 }
 
+/**
+ * Attaches an AudioInterface to this game.
+ * @param newAudio A pointer to the AudioInterface to attach to this game
+ */
 void Module::ModuleGame::attachAudioInterface(AudioInterface* newAudio)
 {
 	newAudio->game = this;
 	audio = newAudio;
 }
 
+/**
+ * Attaches a ThreadingInterface to this game.
+ * @param newThreading A pointer to the ThreadingInterface to attach to this game
+ */
 void Module::ModuleGame::attachThreadingInterface(ThreadingInterface* newThreading)
 {
 	newThreading->game = this;
 	threading = newThreading;
 }
 
+/**
+ * Calls the run function of a ThreadObject in a newly spawned thread.
+ * @param obj The ThreadObject to call the run method of
+ */
 void Module::ModuleGame::startThread(ThreadObject* obj)
 {
 	assert(threading!=0);
 	threading->startThread(obj);
 }
 
+/**
+ * Starts the game.
+ */
 void Module::ModuleGame::start()
 {
 	if(graphics)
@@ -43,6 +69,10 @@ void Module::ModuleGame::start()
 		audio->start();
 }
 
+/**
+ * Checks if the game is running or not.
+ * @return Whether or not the game is currently running
+ */
 bool Module::ModuleGame::isRunning()
 {
 	assert(graphics!=0);
