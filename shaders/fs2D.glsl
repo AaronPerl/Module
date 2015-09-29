b2b5e67cebca6b949e1046a0c72fc606e9f5ae2d
Aaron Perl <perla@rpi.edu>
2015-09-29 14:19:25 -0400

    Removed all files from gh-pages branch

--- shaders/fs2D.glsl
+++
@@ -1,8 +0,0 @@
-#version 150
-
-in vec4 varyingColor;
-
-void main()
-{
-	gl_FragColor = 	varyingColor;
-}
--- shaders/fsnorm.glsl
+++
@@ -1,7 +0,0 @@
-#version 150
-in vec4 vertex_color;
-out vec4 Out_Color;
-void main()
-{
-  Out_Color = vertex_color;
-}
