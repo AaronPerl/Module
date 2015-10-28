26f25e0c1065157fccbe18db832f9ea0d14ec69c
Aaron Perl <perla@rpi.edu>
2015-10-28 04:48:24 -0400

    Implemented 2D polygons

--- include/GraphicsCallback.hpp
+++ include/GraphicsCallback.hpp
@@ -14,13 +14,13 @@ public:
 	 * Called at the beginning of a render cycle, before any objects are drawn.
 	 * @param context A GraphicsContext object associated with the GraphicsInterface that called this hook.
 	 */
-	virtual void onPreRender(const GraphicsContext& context) {}
+	virtual void onPreRender(GraphicsContext& context) {}
 	
 	/**
 	 * Called at the end of a render cycle, after the world has been drawn, but before buffers have been swapped.
 	 * @param context A GraphicsContext object associated with the GraphicsInterface that called this hook.
 	 */
-	virtual void onPostRender(const GraphicsContext& context) {}
+	virtual void onPostRender(GraphicsContext& context) {}
 };
 
 }
--- shaders/vs2D.glsl
+++ shaders/vs2D.glsl
@@ -4,14 +4,14 @@
 
 uniform ivec2 screen_size;
 
-layout (location = 0) in vec3 position;
+layout (location = 0) in vec4 position;
 //layout (location = 1) in vec4 vertex_color;
 
 out vec4 varyingColor;
 
 void main()
 {
-	vec2 scaledPosition = 2 * (vec2(position.x, 0) - vec2(0,position.y))/screen_size;
-    gl_Position = vec4(position.x, -position.y, 0, 0);//vec4(scaledPosition - vec2(1,1),0,0);
-    varyingColor = vec4(1,0,0,1);
+	vec2 scaledPosition = 2 * (position.xy/(vec2(800,600))) - vec2(1,1);
+    gl_Position = vec4(scaledPosition*vec2(1,-1),0,1);
+    varyingColor = vec4(1,1,1,1);
 }
