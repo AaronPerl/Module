577fcb34cb26611f36327f4042894d57caed1214
Aaron Perl <aaronjperl@gmail.com>
2016-03-01 16:57:49 -0500

    Added basic abstract extension class

---
+++ include/Extension.hpp
@@ -0,0 +1,31 @@
+#ifndef __MODULE__EXTENSION_HPP__
+#define __MODULE__EXTENSION_HPP__
+
+#include "ModuleDefines.hpp"
+
+namespace Module {
+
+/**
+ * Extensions are essentially a means to package up the instantiation and
+ * attachment of callback objects, as well as the spawning of threads.
+ * This allows for Module specific libraries that can easily be included in projects.
+ */
+class Extension {
+protected:
+	
+	// IMPLEMENT THESE //
+	virtual const std::string& getName() = 0;
+	virtual const std::string& getVersion() = 0;
+	
+	// These functions are for organizing the instantiation of callbacks and threads
+	virtual void attachGraphicsCallbacks() = 0;
+	virtual void attachInputCallbacks() = 0;
+	virtual void attachAudioCallbacks() = 0;
+	virtual void spawnThreads() = 0;
+public:
+
+};
+
+}
+
+#endif
